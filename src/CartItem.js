2b120d5ad651160b177eb61905c2721effffaa79
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-06 20:38:13 +0530

    Added state in the CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 //CartItem is the class component and we are inheriting from Components class using the extends keyword
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            title: 'Mobile Phone',
+            qty:1,
+            img:''
+        }
+    }
     render(){
+        //object destructuring
+        const {price,title,qty}=this.state;
         //this will return some JsX which will basically describe the UI for that component
         return (
             <div className='cart-item'>
@@ -11,11 +22,23 @@ class CartItem extends React.Component{
                     <img style={styles.image} />
                 </div>
                 <div className='right-block'>
-                    <div style={ {fontSize:25} }>Phone</div>
-                    <div style={ {color: '#777'} }>Rs 999</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
+                    <div style={ {fontSize:25} }>{this.state.title}</div>
+                    <div style={ {color: '#777'} }>Rs {price}</div>
+                    <div style={ {color: '#777'} }>Qty: {qty}</div>
                     <div className='cart-item-actions'>
                         { /* Buttons */}
+                        <img alt="increase" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/992/992651.png" 
+                        />
+                        <img alt="decrease" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
+                        />
+                        <img alt="delete" 
+                        className="action-icons" 
+                        src="https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1651838482~hmac=2fa4719c5dec208e192becf713e9d939" 
+                        />
                     </div>
                 </div>
             </div>
@@ -34,4 +57,4 @@ const styles= {
         background: '#ccc'
     }
 }
-export default CartItem;
+export default CartItem; 
