5bd7a8b78867d6f3afc651b1fe0be8bacf47b49e
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-14 16:34:18 +0530

    added product quantity in the navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,96 @@ import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Cart />
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    // added the state
+    this.state={
+        products:[
+        {
+            price:999,
+            title: 'Watch',
+            qty:1,
+            img:'',
+            id:1
+        },
+        {
+            price:10000,
+            title: 'Mobile Phone',
+            qty:10,
+            img:'',
+            id:2
+        },
+        {
+            price:40000,
+            title: 'Laptop',
+            qty:5,
+            img:'',
+            id:3 
+        }
+    ]
+}
+
+}
+//function to handle the increse in quantity of the products and pass product as the parameter
+handleIncreaseQuantity= (product) =>{
+    console.log('hey please increse the quantity',product);
+    //we need to get the products from array of products
+    const{products}=this.state;
+    //get the index of a particular product
+    const index=products.indexOf(product);
+    products[index].qty+=1;
+    this.setState({
+        products
+    })
+}
+handleDecreaseQuantity= (product) =>{
+    console.log('hey please increse the quantity',product);
+    //we need to get the products from array of products
+    const{products}=this.state;
+    //get the index of a particular product
+    const index=products.indexOf(product);
+
+    if(products[index].qty===0){
+        return;
+    }
+    products[index].qty-=1;
+    this.setState({
+        products
+    })
+}
+handleDeleteProduct=(id)=>{
+    const{products}=this.state;
+    // return another array and the arry will contain products whose id will not be the id at its past
+    const items=products.filter((item)=>item.id!==id)
+    this.setState({
+        products:items
+    })
+}
+getCartCount=()=>{
+  //we will take product from this.state and 
+  //then iterate over each rpoduct and take the count
+  const{products}=this.state;
+  let count=0;
+  products.forEach((product)=>{
+    count += product.qty;
+  })
+  return count;
+}
+
+   render(){
+     const {products}=this.state;
+      return (
+       <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart 
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}/>
+       </div>
+      );
+   }
 }
 
 export default App;
--- src/Cart.js
+++ src/Cart.js
@@ -1,77 +1,10 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-class Cart extends React.Component{
-    constructor(){
-        super();
-        // added the state
-        this.state={
-            products:[
-            {
-                price:999,
-                title: 'Watch',
-                qty:1,
-                img:'',
-                id:1
-            },
-            {
-                price:10000,
-                title: 'Mobile Phone',
-                qty:10,
-                img:'',
-                id:2
-            },
-            {
-                price:40000,
-                title: 'Laptop',
-                qty:5,
-                img:'',
-                id:3 
-            }
-        ]
-    }
-    
-    }
-    //function to handle the increse in quantity of the products and pass product as the parameter
-    handleIncreaseQuantity= (product) =>{
-        console.log('hey please increse the quantity',product);
-        //we need to get the products from array of products
-        const{products}=this.state;
-        //get the index of a particular product
-        const index=products.indexOf(product);
-        products[index].qty+=1;
-        this.setState({
-            products
-        })
-    }
-    handleDecreaseQuantity= (product) =>{
-        console.log('hey please increse the quantity',product);
-        //we need to get the products from array of products
-        const{products}=this.state;
-        //get the index of a particular product
-        const index=products.indexOf(product);
-
-        if(products[index].qty===0){
-            return;
-        }
-        products[index].qty-=1;
-        this.setState({
-            products
-        })
-    }
-    handleDeleteProduct=(id)=>{
-        const{products}=this.state;
-        // return another array and the arry will contain products whose id will not be the id at its past
-        const items=products.filter((item)=>item.id!==id)
-        this.setState({
-            products:items
-        })
-    }
-        // this.testing();
-        // this.increaseQuantity=this.increaseQuantity.bind(this);
-    render(){
+//we are getting the props from React and this props has these products and functions 
+const Cart = (props) => {
         // we can map over this array and use the CartItem component and pass this data as props
-        const {products}=this.state;
+        const {products}=props;
         return (
             <div className='cart'>
                 {/* we can specify props by passing attributes */}
@@ -79,19 +12,17 @@ class Cart extends React.Component{
                 {products.map((product)=>{
                     // each product is given an id to differenciate
                     return (
-                    <CartItem 
-                        product={product} 
-                        key={product.id}
-                        onIncreaseQuantity={this.handleIncreaseQuantity}
-                        onDecreaseQuantity={this.handleDecreaseQuantity}
-                        onDeleteProduct={this.handleDeleteProduct}
-                    />
+                        <CartItem 
+                            product={product} 
+                            key={product.id}
+                            onIncreaseQuantity={props.onIncreaseQuantity}
+                            onDecreaseQuantity={props.onDecreaseQuantity}
+                            onDeleteProduct={props.onDeleteProduct}
+                        />
                     )
                 })}
             </div>
         )
-    }
-    
 }
 
 
